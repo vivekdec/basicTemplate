8cd16919474a247bf7407125b5d4a3b3af1a6a5e
Vivek T <47312387+vivekdec@users.noreply.github.com>
2022-06-06 12:27:59 +0530

    Update contactUs.api.tsx

--- src/services/contactUs.api.tsx
+++ src/services/contactUs.api.tsx
@@ -3,7 +3,7 @@ import nivenUrl from '../values/Strings/NivenUrl';
 export const contactUs = ({name, email_address, telephone, enquiry}: any) => {
   const requestOptions = {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'x-api-key': 1234567890 },
+    headers: { 'Content-Type': 'application/json', 'x-api-key': '' },
     body: JSON.stringify({name, email_address, telephone, enquiry})
   }
   let url = nivenUrl.CONTACT_US_URL;
